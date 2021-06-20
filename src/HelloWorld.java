83c420877792b51a60e95bfef02eb487df708387
afsar074 <afsar.bsansari@gmail.com>
2021-06-20 17:39:03 +0530

    this is another commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
 public static void main(String[] args){
     System.out.println("Hello maven");
     System.out.println("Give me HiFi");
+    System.out.println("this is another commit");
 }
 
 
